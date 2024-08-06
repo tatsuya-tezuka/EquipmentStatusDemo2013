c3c12bfe972049fe90522202da64dbab87e53a21
tatsuya-tezuka <145074813+tatsuya-tezuka@users.noreply.github.com>
2024-08-06 17:43:21 +0900

    Add files via upload

---
+++ EquipmentStatusDemo2013/EquipmentStatusDemo2013Dlg.h
@@ -0,0 +1,32 @@
+
+// EquipmentStatusDemo2013Dlg.h : ヘッダー ファイル
+//
+
+#pragma once
+
+
+// CEquipmentStatusDemo2013Dlg ダイアログ
+class CEquipmentStatusDemo2013Dlg : public CDialogEx
+{
+// コンストラクション
+public:
+	CEquipmentStatusDemo2013Dlg(CWnd* pParent = NULL);	// 標準コンストラクター
+
+// ダイアログ データ
+	enum { IDD = IDD_EQUIPMENTSTATUSDEMO2013_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
