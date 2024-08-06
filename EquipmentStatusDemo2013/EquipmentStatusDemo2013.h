8af247bffd1d35a9008a2fd0507ba81017649811
tatsuya-tezuka <145074813+tatsuya-tezuka@users.noreply.github.com>
2024-08-06 17:47:04 +0900

    Add files via upload

---
+++ EquipmentStatusDemo2013/EquipmentStatusDemo2013.h
@@ -0,0 +1,32 @@
+
+// EquipmentStatusDemo2013.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです。
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'stdafx.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CEquipmentStatusDemo2013App:
+// このクラスの実装については、EquipmentStatusDemo2013.cpp を参照してください。
+//
+
+class CEquipmentStatusDemo2013App : public CWinApp
+{
+public:
+	CEquipmentStatusDemo2013App();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CEquipmentStatusDemo2013App theApp;
